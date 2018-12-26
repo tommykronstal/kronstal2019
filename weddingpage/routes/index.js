5b7ac62e49c1029bab8dda9e2ed471b3595b9637
Tommy Kronstal <tommy.kronstal@gmail.com>
2018-12-26 13:27:23 +0100

    new text on index

--- weddingpage/routes/index.js
+++ weddingpage/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Anni & Tommy 2019' });
+  res.render('index', { title: 'Anni & Tommy' });
 });
 
 /* GET home page. */
