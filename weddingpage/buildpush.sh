38d94b155aea3d497e847abf02471ad7d26c4783
Tommy Kronstal <tommy.kronstal@gmail.com>
2018-12-26 14:04:49 +0100

    deploy after building and pushing

--- weddingpage/buildpush.sh
+++ weddingpage/buildpush.sh
@@ -2,3 +2,5 @@
 
 docker build -t tommykronstal/weddingpage:latest .
 docker push tommykronstal/weddingpage:latest
+
+ssh -i ~/.ssh/MyKeyPair.pem ubuntu@kronstal.se '/home/ubuntu/deploy-weddingpage.sh'
